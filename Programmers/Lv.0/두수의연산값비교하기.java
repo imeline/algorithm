2696b6ca731fb02c0a529156ffdcf7952a0517a4
sunju <iseonju745@gmail.com>
2023-11-23 00:19:49 +0900

    Lv.0

---
+++ Programmers/Lv.0/두수의연산값비교하기.java
@@ -0,0 +1,11 @@
+public class 두수의연산값비교하기 {
+    public int solution(int a, int b) {
+        String strA_B =  Integer.toString(a) + Integer.toString(b);;
+        int a_b = Integer.parseInt(strA_B);
+
+        return (a_b >= 2*a*b) ? a_b : 2*a*b;
+    }
+    // 더 좋은 풀이
+    // int a_b = Integer.parseInt(a + "" + b);
+    // return Math.max(a_b, 2*a*b);
+}
