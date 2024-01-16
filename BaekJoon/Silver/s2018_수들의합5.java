28f2915045a5452b7b58129536aadab6e88191b9
sunju <iseonju745@gmail.com>
2024-01-16 23:00:20 +0900

    투포인터

--- BaekJoon/Silver/s2018_수들의합5.java
+++ BaekJoon/Silver/s2018_수들의합5.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-public class p2018_수들의합5 {
+public class s2018_수들의합5 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int target = Integer.parseInt(br.readLine());
