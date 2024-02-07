cfc94e94a3da233bcefcb9c2dddce5e53fa9b9d0
sunju <iseonju745@gmail.com>
2024-02-07 15:31:59 +0900

    이진 탐색

---
+++ BaekJoon/Silver/s1920_수찾기/s1920_수찾기.java
@@ -0,0 +1,45 @@
+package BaekJoon.Silver.s1920_수찾기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class s1920_수찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<m; i++){
+            int tarket = Integer.parseInt(st.nextToken());
+            int start = 0;
+            int end = nums.length - 1; // m-1 했더니 ArrayIndexOutOfBounds 에러 남
+            boolean find = false;
+            while (start <= end){
+                int mid = (start+end) / 2;
+                if(nums[mid] > tarket) end = mid - 1;
+                else if(nums[mid] < tarket) start = mid + 1;
+                else {
+                    find = true;
+                    break;
+                }
+            }
+            if(find) sb.append(1).append("\n");
+            else sb.append(0).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
