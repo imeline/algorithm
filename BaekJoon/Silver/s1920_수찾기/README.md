d2b910ae6919b55ec657126dfa01517fe172bbad
sunju <iseonju745@gmail.com>
2024-02-15 10:49:37 +0900

    이진 탐색

--- BaekJoon/Silver/s1920_수찾기/README.md
+++ BaekJoon/Silver/s1920_수찾기/README.md
@@ -1,12 +1,15 @@
 # 문제
+
 [백준 1920번 - 수 찾기](https://www.acmicpc.net/problem/1920)
 
-## 알고리즘 
+## 알고리즘
+
 - 이진탐색 <br>
--> 처음 접하는 개념이라 강의의 수도 코드 본 후 풀어 봄
+  -> 처음 접하는 개념이라 강의의 수도 코드 본 후 풀어 봄
 
 # 문제 풀이
-1. 데이터 배열 정렬 
+
+1. 데이터 배열 정렬
 2. 찾아야하는 수(타깃 데이터)의 갯수만큼 for문
 3. start 값 <= end 값 까지 while
 4. 중앙 인덱스 배정
@@ -15,10 +18,7 @@
 7. 그 밖의 else는 중앙 값 = 타깃 값 을 뜻하므로, boolean변수 true 만들고 반복문 종료
 8. boolean 변수에 따라 1, 0 중 출력
 
-## 주의점
-- 처음 `end` 를 `nums.length - 1` 말고 `m-1`을 줬더니, <br>
-  ArrayIndexOutOfBounds 에러가 남
-
 ## 참고 강의
+
 [Do it! 알고리즘 코딩테스트 with JAVA](https://www.inflearn.com/course/lecture?courseSlug=두잇-알고리즘-코딩테스트-자바&unitId=148354&tab=curriculum)
 
--- BaekJoon/Silver/s1920_수찾기/s1920_수찾기.java
+++ BaekJoon/Silver/s1920_수찾기/s1920_수찾기.java
@@ -15,7 +15,7 @@ public class s1920_수찾기 {
         st = new StringTokenizer(br.readLine());
 
         int[] nums = new int[n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nums);
@@ -23,21 +23,21 @@ public class s1920_수찾기 {
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             int tarket = Integer.parseInt(st.nextToken());
             int start = 0;
-            int end = nums.length - 1; // m-1 했더니 ArrayIndexOutOfBounds 에러 남
+            int end = nums.length - 1;
             boolean find = false;
-            while (start <= end){
-                int mid = (start+end) / 2;
-                if(nums[mid] > tarket) end = mid - 1;
-                else if(nums[mid] < tarket) start = mid + 1;
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (nums[mid] > tarket) end = mid - 1;
+                else if (nums[mid] < tarket) start = mid + 1;
                 else {
                     find = true;
                     break;
                 }
             }
-            if(find) sb.append(1).append("\n");
+            if (find) sb.append(1).append("\n");
             else sb.append(0).append("\n");
         }
         System.out.println(sb);
