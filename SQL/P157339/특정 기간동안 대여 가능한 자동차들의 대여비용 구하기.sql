53bf76b61e60ae55b4fce3d6d67e11f787c8df9a
LEE SUNJU <128706341+imeline@users.noreply.github.com>
2024-11-20 21:01:46 +0900

    JOIN

--- SQL/P157339/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ SQL/P157339/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -4,11 +4,12 @@ from CAR_RENTAL_COMPANY_CAR c
 join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p on c.CAR_TYPE = p.CAR_TYPE
                                     
 where c.CAR_TYPE in ('세단', 'SUV') 
-    and c.CAR_ID not in (
-        select distinct CAR_ID
-        from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        where START_DATE <= '2022-11-30' and END_DATE >= '2022-11-01'
-        ) 
+    and NOT EXISTS (
+        select 1
+        from CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+        where h.CAR_ID = c.CAR_ID
+        and h.START_DATE <= '2022-11-30' AND h.END_DATE >= '2022-11-01'
+    )
     and p.DURATION_TYPE = '30일 이상'
     and round(30 * c.DAILY_FEE * (1 - p.DISCOUNT_RATE / 100)) between 500000 and 1999999
                                     
