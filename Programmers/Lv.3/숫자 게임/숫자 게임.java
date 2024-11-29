41cf8d4711a4bec3783e17f9d7b8a4c13a1b19ea
LEE SUNJU <128706341+imeline@users.noreply.github.com>
2024-11-29 21:23:42 +0900

    그리디

---
+++ Programmers/Lv.3/숫자 게임/숫자 게임.java
@@ -0,0 +1,14 @@
+public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+        for (int i = A.length - 1, j = B.length - 1; i >= 0; i--) {
+            if(A[i] < B[j]) {
+                answer++;
+                j--;
+            }
+        }
+
+        return answer;
+}
